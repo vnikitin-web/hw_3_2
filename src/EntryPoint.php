01b2e128e65ab034cf642ed1d37549e55082c8c4
Vladimir Nikitin <v.nikitin@squesito.ru>
2021-02-23 16:19:28 +0300

    23022021

---
+++ src/EntryPoint.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace MyPackage;
+
+class EntryPoint
+{
+	public function __construct()
+	{
+		
+	}
+
+	public function start()
+	{
+		echo "OTUS Home Work. Lesson 3, work 2";
+	}
+}
+
